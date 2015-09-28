d1525985658826db12db41339921897ffef993a8
sashockster <shockley.shaina@gmail.com>
2015-09-27 22:16:21 -0700

    added first Bonfire

---
+++ Basic Algorithm Scripting/1 Meet Bonfire.js
@@ -0,0 +1,9 @@
+function meetBonfire(argument) {
+  // Good luck!
+  console.log("you can read this function's argument in the developer tools", argument);
+
+  return true;
+}
+
+
+meetBonfire("You can do this!");
